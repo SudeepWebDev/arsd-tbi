8f117414d59a50ec7f91de8feec6017f3684283a
SudeepWebDev <127820890+SudeepWebDev@users.noreply.github.com>
2023-11-01 00:02:15 +0530

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const toggleButton = document.getElementById('menuToggle');
 const navMenu = document.getElementById('navbarNav');
 const menu_btn_3line = document.getElementById('menu-btn-3line');
 const menu_btn_xmark = document.getElementById('menu-btn-xmark');
+const theme_toggler = document.getElementById('theme-toggler');
+const sun_icon = document.getElementById('sun-icon');
+const moon_icon = document.getElementById('moon-icon');
 if (toggleButton) {
 
     toggleButton.addEventListener('click', function () {
@@ -13,6 +16,21 @@ if (toggleButton) {
             navMenu.classList.add('hidden');
             menu_btn_xmark.classList.add('hidden');
             menu_btn_3line.classList.remove('hidden');
+
+        }
+    });
+}
+
+if (theme_toggler) {
+    theme_toggler.addEventListener('click', function () {
+
+        if (sun_icon.classList.contains('hidden')) {
+            sun_icon.classList.remove('hidden');
+            moon_icon.classList.add('hidden');
+        } else {
+            sun_icon.classList.add('hidden');
+            moon_icon.classList.remove('hidden');
+            // document.documentElement.classList.toggle('dark');
 
         }
     });
